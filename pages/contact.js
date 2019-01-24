846a17fd461701cf8a35de175eb53aa49384fe74
Vishal Sharma <vish.sharma0203@gmail.com>
2019-01-24 18:02:00 +0530

    added contact page layout

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,16 @@
+import { Container } from '../components/styles/Layout'
+import { Content, PageTitle } from '../components/styles/GeneralStyles'
+import { colors } from '../components/styles/GlobalStyles'
+
 const Contact = () => (
-  <div>
-  </div>
+  <Container>
+    <PageTitle textColor={colors.app.primary}>
+      Contact
+    </PageTitle>
+    <Content>
+
+    </Content>
+  </Container>
 );
 
 export default Contact;
