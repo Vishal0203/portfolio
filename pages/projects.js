df348c3455933a9327bfefbebcee27fc6916af1a
Vishal Sharma <vish.sharma0203@gmail.com>
2019-01-27 09:25:17 +0530

    updated project packages

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,21 @@
+import styled from 'styled-components'
+import { Container } from '../components/styles/Layout'
+import { colors } from '../components/styles/GlobalStyles'
+import { PageTitle } from '../components/styles/GeneralStyles'
+import { Content } from '../components/styles/GeneralStyles'
+
+const CenteredContent = styled(Content)`
+  text-align: center;
+`
+
 const Projects = () => (
-  <div>
-  </div>
+  <Container>
+    <PageTitle textColor={colors.app.primary}>
+      Projects
+    </PageTitle>
+    <CenteredContent>
+    </CenteredContent>
+  </Container>
 );
 
 export default Projects;
