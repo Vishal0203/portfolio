faf280db8d78285ae6debc915181301ba44cae78
Vishal Sharma <vish.sharma0203@gmail.com>
2019-09-21 03:55:52 +0530

    added gtag

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App {...props} />),
     );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags }
@@ -16,6 +16,21 @@ class MyDocument extends Document {
       <html>
         <Head>
           {this.props.styleTags}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-148403885-1">
+          </script>
+          <script dangerouslySetInnerHTML={
+            {
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments)}
+                gtag("js", new Date());
+                gtag("config", "UA-148403885-1");
+              `,
+            }
+          }>
+          </script>
         </Head>
         <body>
           <Main />
